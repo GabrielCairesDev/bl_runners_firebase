aa0fd5e6f9018ff2eeb1bdb3bcb11dc7e96d38f8
Gabriel <gabriel.araujo.caires@gmail.com>
2023-11-09 21:50:22 -0300

    1.0.2

--- lib/utils/utilitarios.dart
+++ lib/utils/utilitarios.dart
@@ -1,30 +1,31 @@
 class Utilitarios {
-  String calcularRitmo(int? metros, int? minutos) {
-    if (metros == null || metros == 0 || minutos == null || minutos == 0) {
+  String calcularRitmo(int? metros, int? segundos) {
+    if (metros == null || metros == 0 || segundos == null || segundos == 0) {
       return '00:00/km';
     }
 
     double quilometros = metros / 1000.0;
-    double ritmoMinutos = minutos / quilometros;
+    double ritmoSegundos = segundos / quilometros;
 
-    if (ritmoMinutos.isInfinite || ritmoMinutos.isNaN) {
+    if (ritmoSegundos.isInfinite || ritmoSegundos.isNaN) {
       return '00:00/km';
     }
 
-    int minutosInteiros = ritmoMinutos.toInt();
-    int segundosInteiros = ((ritmoMinutos - minutosInteiros) * 60).toInt();
+    int minutosInteiros = ritmoSegundos ~/ 60;
+    int segundosInteiros = (ritmoSegundos % 60).toInt();
 
-    String resultado = '${minutosInteiros.toString().padLeft(2, '0')}:${segundosInteiros.toString().padLeft(2, '0')} /km';
+    String resultado =
+        '${minutosInteiros.toString().padLeft(2, '0')}:${segundosInteiros.toString().padLeft(2, '0')} /km';
     return resultado;
   }
 
-  String formatarTempo(int? minutos) {
-    if (minutos == null) {
+  String formatarTempo(int? segundos) {
+    if (segundos == null) {
       return '0h 0min';
     } else {
-      int horas = minutos ~/ 60;
-      int minutosRestantes = minutos % 60;
-      return '${horas}h ${minutosRestantes}min';
+      int horas = segundos ~/ 3600;
+      int minutos = (segundos % 3600) ~/ 60;
+      return '${horas}h ${minutos}min';
     }
   }
 
